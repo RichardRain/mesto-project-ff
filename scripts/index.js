96aed79f8ebbf7a0a7a9f15e94bf2e09d6c78059
RichardRain <alexander.hell.scorpion@gmail.com>
2024-01-08 17:20:38 +0300

    Внесены правки

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,16 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
 let cardToAdd;
 
-function addCard(cardImage, cardTitle, removeCard) {
+function makeCard(cardImage, cardTitle, removeCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardRemoveButton = cardElement.querySelector('.card__delete-button');
+  const cardImageSource = cardElement.querySelector('.card__image').src;
+  const cardImageDescription = cardElement.querySelector('.card__image').alt;
+  const cardTitleText = cardElement.querySelector('.card__title').textContent;
 
-  cardElement.querySelector('.card__image').src = cardImage;
-  cardElement.querySelector('.card__image').alt = cardTitle;
-  cardElement.querySelector('.card__title').textContent = cardTitle;
+  cardImageSource = cardImage;
+  cardImageDescription = cardTitle;
+  cardTitleText = cardTitle;
 
   cardRemoveButton.addEventListener('click', () => removeCard(cardElement));
 
@@ -20,6 +23,6 @@ function removeCard(cardToDelete) {
 }
 
 initialCards.forEach((item) => {
-  cardToAdd = addCard(item.link, item.name, removeCard);
+  cardToAdd = makeCard(item.link, item.name, removeCard);
   cardList.append(cardToAdd);
 });
